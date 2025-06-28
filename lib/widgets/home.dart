d1e515f966bad9d49f3a7b128d773fa0487d44a0
fardeenhimself <fardeenviper77@gmail.com>
2025-06-29 00:51:45 +0600

    Removed item can be undone

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/main.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -38,7 +39,7 @@ class _HomeState extends State<Home> {
     );
   }
 
-  //Creating the new expense method that adds a new expense from the input to the list
+  //Creating the new expense method that adds a new expense from the input to the list;
   void addNewExpense(Expense expense) {
     setState(() {
       _registeredExpense.add(expense);
@@ -47,13 +48,46 @@ class _HomeState extends State<Home> {
 
   //Remove a expense from the UI and _registeredList
   void removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpense.indexOf(
+      expense,
+    ); //.insert needs index and item. index of expense from the list is stored here;
     setState(() {
       _registeredExpense.remove(expense);
     });
+
+    //clears the snackbar;
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    //shows a snackbar and lets user do something with a button;
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        content: Text('Expense removed.'),
+        action: SnackBarAction(
+          label: "Undo Change",
+          onPressed: () {
+            setState(() {
+              _registeredExpense.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    // This condition checks if the registered list is empty or not. If yes it sets the mainContent to ExpensesList.
+    Widget mainContent = Center(
+      child: Text('List is empty. Add by clicking the "+" icon.'),
+    );
+
+    if (_registeredExpense.isNotEmpty) {
+      mainContent = ExpenseList(
+        onRemoveExpense: removeExpense,
+        expenses: _registeredExpense,
+      );
+    }
     return Scaffold(
       //App Bar
       appBar: AppBar(
@@ -67,12 +101,7 @@ class _HomeState extends State<Home> {
         child: Column(
           children: [
             Text('Chart'),
-            Expanded(
-              child: ExpenseList(
-                onRemoveExpense: removeExpense,
-                expenses: _registeredExpense,
-              ),
-            ),
+            Expanded(child: mainContent),
           ],
         ),
       ),
